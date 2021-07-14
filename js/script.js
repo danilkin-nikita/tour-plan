f90c75fee3fa8fbf950af2e0eb95f6aef8cc470e
Nikita Danilkin <ni.danilkin@gmail.com>
2021-07-14 15:00:01 +0300

    create: compled hotel section and connection Yandex Maps API

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 "use sctrict";
-
+//Инициализация слайдера отеля
 const hotelSwiper = new Swiper(".hotel-slider", {
   loop: true,
   navigation: {
@@ -8,9 +8,44 @@ const hotelSwiper = new Swiper(".hotel-slider", {
   },
   grabCursor: true,
   autoplay: {
-    delay: 5000,
+    delay: 4000,
+    disableOnInteraction: false,
   },
   keyboard: {
     enabled: true,
   },
 });
+
+//Инициализация карты
+ymaps.ready(init);
+
+function init() {
+  // Создание карты.
+  let myMap = new ymaps.Map(
+    "map",
+    {
+      center: [7.8381759, 98.29881076],
+      zoom: 17,
+      controls: ["smallMapDefaultSet"],
+    },
+    {
+      restrictMapArea: [
+        [7.86936196, 98.2785118],
+        [7.8028573, 98.31533317],
+      ],
+    }
+  );
+  myMap.geoObjects.add(
+    new ymaps.Placemark(
+      [7.8381759, 98.29881076],
+      {
+        balloonContent: "GRAND HILTON HOTEL",
+        iconCaption: "HILTON",
+      },
+      {
+        preset: "islands#blueCircleDotIconWithCaption",
+        iconCaptionMaxWidth: "50",
+      }
+    )
+  );
+}
