208252bf805c8a6d46810b898b5f9a1238e9c417
Nikita Danilkin <ni.danilkin@gmail.com>
2021-07-28 22:08:57 +0300

    site optimization

--- js/script.js
+++ js/script.js
@@ -1,169 +1,199 @@
-"use sctrict";
-// Скрипт для мобильного меню
-const toogleMobileMenu = () => {
-  let menuButton = document.querySelector(".menu-button"),
-    navbarMenu = document.querySelector(".navbar-menu");
+window.addEventListener("DOMContentLoaded", () => {
+  ("use sctrict");
 
-  menuButton.addEventListener("click", () => {
-    navbarMenu.classList.toggle("navbar-menu--visible");
-    menuButton.classList.toggle("menu-button--active");
-    document.body.classList.toggle("scroll-menu");
-  });
-};
+  // Скрипт для мобильного меню
+  const toogleMobileMenu = () => {
+    let menuButton = document.querySelector(".menu-button"),
+      navbarMenu = document.querySelector(".navbar-menu");
 
-toogleMobileMenu();
-
-// Инициализация слайдеров
-const initSliders = () => {
-  // Инициализация слайдера отеля
-  const hotelSwiper = new Swiper(".hotel-slider", {
-    loop: true,
-    navigation: {
-      nextEl: ".hotel-slider__button--next",
-      prevEl: ".hotel-slider__button--prev",
-    },
-    grabCursor: true,
-    autoplay: {
-      delay: 4000,
-      disableOnInteraction: false,
-    },
-    keyboard: {
-      enabled: true,
-    },
-  });
-  // Инициализация слайдера с отзывами
-  const reviewsSwiper = new Swiper(".reviews-slider", {
-    loop: true,
-    navigation: {
-      nextEl: ".reviews-slider__button--next",
-      prevEl: ".reviews-slider__button--prev",
-    },
-  });
-};
-
-initSliders();
-
-const init = () => {
-  // Создание карты.
-  let myMap = new ymaps.Map(
-    "map",
-    {
-      center: [7.8381759, 98.29881076],
-      zoom: 17,
-      controls: ["smallMapDefaultSet"],
-    },
-    {
-      // Ограничение области карты
-      restrictMapArea: [
-        [7.86936196, 98.2785118],
-        [7.8028573, 98.31533317],
-      ],
-    }
-  );
-  // Создание точки на карте
-  myMap.geoObjects.add(
-    new ymaps.Placemark(
-      [7.8381759, 98.29881076],
-      {
-        balloonContent: "GRAND HILTON HOTEL",
-        iconCaption: "HILTON",
-      },
-      {
-        preset: "islands#blueCircleDotIconWithCaption",
-        iconCaptionMaxWidth: "50",
-      }
-    )
-  );
-};
-
-//Инициализация карты
-if (document.querySelector(".map")) {
-  ymaps.ready(init);
-}
-
-// Открытие модального окна
-const toogleModal = () => {
-  const modalOverlay = document.querySelector(".modal__overlay"),
-    modalDialog = document.querySelector(".modal__dialog");
-
-  const openModal = () => {
-    modalOverlay.classList.add("modal__overlay--visible");
-    modalDialog.classList.add("modal__dialog--visible");
+    menuButton.addEventListener("click", () => {
+      navbarMenu.classList.toggle("navbar-menu--visible");
+      menuButton.classList.toggle("menu-button--active");
+      document.body.classList.toggle("scroll-menu");
+    });
   };
 
-  const closeModal = () => {
-    modalOverlay.classList.remove("modal__overlay--visible");
-    modalDialog.classList.remove("modal__dialog--visible");
-  };
+  toogleMobileMenu();
 
-  document.addEventListener("click", (event) => {
-    let target = event.target;
-
-    if (target.matches('button[data-toggle="modal"]')) {
-      openModal();
-    }
-
-    if (target.closest(".modal__close") || target.matches(".modal__overlay")) {
-      closeModal();
-    }
-  });
-
-  document.addEventListener("keydown", (event) => {
-    if (
-      event.keyCode === 27 &&
-      modalDialog.classList.contains("modal__dialog--visible")
-    ) {
-      closeModal();
-    }
-  });
-};
-
-toogleModal();
-
-// Создание параллакс эффекта для блока newsletter
-$(".newsletter").parallax({
-  imageSrc: "img/newsletter/newsletter-bg.jpeg",
-  speed: 0.4,
-});
-
-//Валидация форм
-$(".send-form").each(function () {
-  $(this).validate({
-    errorClass: "invalid",
-    messages: {
-      name: {
-        required: "Please specify your name",
-        minlength: "Name must be at least 2 letters",
+  // Инициализация слайдеров
+  const initSliders = () => {
+    // Инициализация слайдера отеля
+    const hotelSwiper = new Swiper(".hotel-slider", {
+      loop: true,
+      navigation: {
+        nextEl: ".hotel-slider__button--next",
+        prevEl: ".hotel-slider__button--prev",
       },
-      email: {
-        required: "Please enter your email",
-        email: "Email format: name@domain.com",
+      grabCursor: true,
+      autoplay: {
+        delay: 4000,
+        disableOnInteraction: false,
       },
-      phone: {
-        required: "Please enter your phone number",
-        minlength: "Please enter the full phone number",
-      },
-    },
-  });
-});
-
-//Подключение маски для телефона
-$(".input[name=phone]").mask("+7 (000) 000-00-00");
-
-AOS.init({
-  disable: function () {
-    let maxWidth = 1000;
-    return window.innerWidth < maxWidth;
-  },
-});
-
-window.addEventListener("resize", () => {
-  if (window.innerWidth < 1000) {
-    AOS.init({
-      disable: function () {
-        let maxWidth = 1000;
-        return window.innerWidth < maxWidth;
+      keyboard: {
+        enabled: true,
       },
     });
+    // Инициализация слайдера с отзывами
+    const reviewsSwiper = new Swiper(".reviews-slider", {
+      loop: true,
+      navigation: {
+        nextEl: ".reviews-slider__button--next",
+        prevEl: ".reviews-slider__button--prev",
+      },
+    });
+  };
+
+  initSliders();
+
+  const init = () => {
+    // Создание карты.
+    let myMap = new ymaps.Map(
+      "map",
+      {
+        center: [7.8381759, 98.29881076],
+        zoom: 17,
+        controls: ["smallMapDefaultSet"],
+      },
+      {
+        // Ограничение области карты
+        restrictMapArea: [
+          [7.86936196, 98.2785118],
+          [7.8028573, 98.31533317],
+        ],
+      }
+    );
+    // Создание точки на карте
+    myMap.geoObjects.add(
+      new ymaps.Placemark(
+        [7.8381759, 98.29881076],
+        {
+          balloonContent: "GRAND HILTON HOTEL",
+          iconCaption: "HILTON",
+        },
+        {
+          preset: "islands#blueCircleDotIconWithCaption",
+          iconCaptionMaxWidth: "50",
+        }
+      )
+    );
+  };
+
+  // Скрипт для загрузки карты по запросу
+  const lazyLoadingMap = () => {
+    let map = document.querySelector(".map");
+    let optionsMap = {
+      once: true,
+      passive: true,
+      capture: true,
+    };
+
+    let mapLoaded = false;
+
+    const startLazyMap = () => {
+      if (!mapLoaded) {
+        mapLoaded = true;
+        ymaps.ready(init);
+      }
+    };
+
+    map.addEventListener("click", startLazyMap, optionsMap);
+    map.addEventListener("mouseover", startLazyMap, optionsMap);
+    map.addEventListener("touchstart", startLazyMap, optionsMap);
+    map.addEventListener("touchmove", startLazyMap, optionsMap);
+  };
+
+  //Инициализация карты
+  if (document.querySelector(".map")) {
+    lazyLoadingMap();
   }
+
+  // Открытие модального окна
+  const toogleModal = () => {
+    const modalOverlay = document.querySelector(".modal__overlay"),
+      modalDialog = document.querySelector(".modal__dialog");
+
+    const openModal = () => {
+      modalOverlay.classList.add("modal__overlay--visible");
+      modalDialog.classList.add("modal__dialog--visible");
+    };
+
+    const closeModal = () => {
+      modalOverlay.classList.remove("modal__overlay--visible");
+      modalDialog.classList.remove("modal__dialog--visible");
+    };
+
+    document.addEventListener("click", (event) => {
+      let target = event.target;
+
+      if (target.matches('button[data-toggle="modal"]')) {
+        openModal();
+      }
+
+      if (
+        target.closest(".modal__close") ||
+        target.matches(".modal__overlay")
+      ) {
+        closeModal();
+      }
+    });
+
+    document.addEventListener("keydown", (event) => {
+      if (
+        event.keyCode === 27 &&
+        modalDialog.classList.contains("modal__dialog--visible")
+      ) {
+        closeModal();
+      }
+    });
+  };
+
+  toogleModal();
+
+  // Создание параллакс эффекта для блока newsletter
+  $(".newsletter").parallax({
+    imageSrc: "img/newsletter/newsletter-bg.jpg",
+    speed: 0.4,
+  });
+
+  //Валидация форм
+  $(".send-form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "Name must be at least 2 letters",
+        },
+        email: {
+          required: "Please enter your email",
+          email: "Email format: name@domain.com",
+        },
+        phone: {
+          required: "Please enter your phone number",
+          minlength: "Please enter the full phone number",
+        },
+      },
+    });
+  });
+
+  //Подключение маски для телефона
+  $(".input[name=phone]").mask("+7 (000) 000-00-00");
+
+  AOS.init({
+    disable: function () {
+      let maxWidth = 1000;
+      return window.innerWidth < maxWidth;
+    },
+  });
+
+  window.addEventListener("resize", () => {
+    if (window.innerWidth < 1000) {
+      AOS.init({
+        disable: function () {
+          let maxWidth = 1000;
+          return window.innerWidth < maxWidth;
+        },
+      });
+    }
+  });
 });
